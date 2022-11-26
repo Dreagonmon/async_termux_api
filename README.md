47e018136f70026a0b49b19f343491b9fd18c266
Dreagonmon <531486058@qq.com>
2022-11-26 13:12:39 +0800

    more termux api wrapper

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ async termux api
 apt install termux-api
 # you need to install android-tools if you want to use adb
 apt install android-tools
+# you need to install tsu if you want to start wireless adb with root
+apt install tsu
 """
--- termux.py
+++ termux.py
@@ -1,6 +1,7 @@
-import asyncio, os, uuid, shlex, atexit
+import asyncio, os, uuid, shlex, atexit, json
 from typing import Optional, MutableSet, Callable, Coroutine
 ActionCallback = Callable[[], Coroutine]
+GROUP_NUMBER_INPUT = "_number_input_"
 ACTION_CLICK = "click"
 ACTION_DELETE = "delete"
 ACTION_BUTTON1 = "button1"
@@ -11,6 +12,8 @@ ACTION_MEDIA_PAUSE = "media_pause"
 ACTION_MEDIA_NEXT = "media_next"
 ACTION_MEDIA_PREVIOUS = "media_previous"
 async def NO_OP(): pass
+BRACE_OPEN = b"{"[0]
+BRACE_CLOSE = b"}"[0]
 
 def _is_in_termux():
     return os.environ.get("TMPDIR", "/root").startswith("/data/data/com.termux")
@@ -26,6 +29,35 @@ def _find_in_path(file, path=None):
             if os.path.isfile(f_path):
                 return f_path
     return None
+TERMUX_ROOTDIR = "/data/data/com.termux/files"
+TERMUX_HOMEDIR = os.path.join(TERMUX_ROOTDIR, "home")
+DEFAULT_TMPDIR = os.path.join(TERMUX_ROOTDIR, "usr", "tmp")
+DEFAULT_PATH = os.path.join(TERMUX_ROOTDIR, "usr", "bin")
+SYSTEM_PATH = "/system/bin"
+TMPDIR = os.environ.get("TMPDIR", DEFAULT_TMPDIR) if _is_in_termux() else DEFAULT_TMPDIR
+PATH = os.environ.get("PATH", DEFAULT_PATH) if _is_in_termux() else DEFAULT_PATH
+TERMUX_TOAST = _find_in_path("termux-toast", PATH)
+TERMUX_NOTIFICATION = _find_in_path("termux-notification", PATH)
+TERMUX_NOTIFICATION_REMOVE = _find_in_path("termux-notification-remove", PATH)
+TERMUX_CLIPBOARD_GET = _find_in_path("termux-clipboard-get", PATH)
+TERMUX_CLIPBOARD_SET = _find_in_path("termux-clipboard-set", PATH)
+TERMUX_BATTERY_STATUS = _find_in_path("termux-battery-status", PATH)
+TERMUX_FINGERPRINT = _find_in_path("termux-fingerprint", PATH)
+TERMUX_VIBRATE = _find_in_path("termux-vibrate", PATH)
+TERMUX_MEDIA_PLAYER = _find_in_path("termux-media-player", PATH)
+TERMUX_SENSOR = _find_in_path("termux-sensor", PATH)
+ADB = _find_in_path("adb", PATH)
+CURL = _find_in_path("curl", PATH)
+SUDO = _find_in_path("sudo", PATH)
+AD_START = os.path.join(SYSTEM_PATH, "start")
+AD_STOP = os.path.join(SYSTEM_PATH, "stop")
+
+__background_tasks = set()
+
+def _create_bg_task(coro):
+    task = asyncio.create_task(coro)
+    __background_tasks.add(task)
+    task.add_done_callback(__background_tasks.discard)
 
 async def _run(cmd):
     if not isinstance(cmd, str):
@@ -53,13 +85,62 @@ async def toast(msg, background:str="gray", color:str="white", position:str="mid
     args.append(msg)
     await _run(args)
 
+async def vibrate(duration = 1000, force = False):
+    args = [TERMUX_VIBRATE, "-d", str(duration)]
+    if force:
+        args.append("-f")
+    await _run(args)
+
 async def get_clipboard():
     """ get clipboard, may not work """
     res = await _run([TERMUX_CLIPBOARD_GET])
-    return res[1].decode("utf8") if res[1] else ""
+    return res[1].decode("utf8").rstrip("\r\n") if res[1] else ""
 
 async def set_clipboard(text):
     await _run([TERMUX_CLIPBOARD_SET, text])
+
+async def battery_status():
+    res = await _run([TERMUX_BATTERY_STATUS])
+    result = res[1].decode("utf8") if res[1] else "{}"
+    return json.loads(result)
+
+async def check_fingerprint():
+    res = await _run([TERMUX_FINGERPRINT])
+    result = res[1].decode("utf8") if res[1] else ""
+    return result.find("AUTH_RESULT_SUCCESS") >= 0
+
+async def media_play(file: Optional[str] = None):
+    args = [TERMUX_MEDIA_PLAYER, "play"]
+    if isinstance(file, str):
+        args.append(file)
+    await _run(args)
+
+async def media_pause():
+    await _run([TERMUX_MEDIA_PLAYER, "pause"])
+
+async def media_stop():
+    await _run([TERMUX_MEDIA_PLAYER, "stop"])
+
+async def media_info():
+    res = await _run([TERMUX_MEDIA_PLAYER, "info"])
+    result = res[1].decode("utf8").rstrip("\r\n") if res[1] else ""
+    info = {}
+    for line in result.splitlines():
+        inx = line.find(":")
+        if inx < 0:
+            continue
+        param = line[:inx].lower().replace(" ", "_")
+        if param == "current_position":
+            value = tuple(line[inx+2:].split(" / "))
+        else:
+            value = line[inx+2:]
+        info[param] = value
+    return info
+
+async def sensor_list():
+    res = await _run([TERMUX_SENSOR, "-l"])
+    result = res[1].decode("utf8") if res[1] else "{}"
+    return json.loads(result).get("sensors", [])
 
 class Notification:
     def __init__(self, n_id:int, content:str, title:str="", *, group:str="", priority:str="", n_type:str="", alert_once:bool=False, ongoing:bool=False, sound:bool=False, image_path:str="", icon:str="", vibrate:str=""):
@@ -169,7 +250,7 @@ class NotificationManager:
         await _run(cmd)
     
     async def send_notification_wait(self, notification_item: Notification):
-        asyncio.create_task(self.send_notification(notification_item))
+        _create_bg_task(self.send_notification(notification_item))
         await notification_item._flag.wait()
         return notification_item._result
     
@@ -200,15 +281,15 @@ class NotificationManager:
             if n.n_id == nid:
                 break
         else: return # not found, do nothing
-        if action == ACTION_CLICK and n.action_click: asyncio.create_task(n.action_click())
-        elif action == ACTION_DELETE and n.action_delete: asyncio.create_task(n.action_delete())
-        elif action == ACTION_MEDIA_PLAY and n.action_media_play: asyncio.create_task(n.action_media_play())
-        elif action == ACTION_MEDIA_PAUSE and n.action_media_pause: asyncio.create_task(n.action_media_pause())
-        elif action == ACTION_MEDIA_NEXT and n.action_media_next: asyncio.create_task(n.action_media_next())
-        elif action == ACTION_MEDIA_PREVIOUS and n.action_media_previous: asyncio.create_task(n.action_media_previous())
-        elif action == ACTION_BUTTON1 and n.action_button1: asyncio.create_task(n.action_button1())
-        elif action == ACTION_BUTTON2 and n.action_button2: asyncio.create_task(n.action_button2())
-        elif action == ACTION_BUTTON3 and n.action_button3: asyncio.create_task(n.action_button3())
+        if action == ACTION_CLICK and n.action_click: _create_bg_task(n.action_click())
+        elif action == ACTION_DELETE and n.action_delete: _create_bg_task(n.action_delete())
+        elif action == ACTION_MEDIA_PLAY and n.action_media_play: _create_bg_task(n.action_media_play())
+        elif action == ACTION_MEDIA_PAUSE and n.action_media_pause: _create_bg_task(n.action_media_pause())
+        elif action == ACTION_MEDIA_NEXT and n.action_media_next: _create_bg_task(n.action_media_next())
+        elif action == ACTION_MEDIA_PREVIOUS and n.action_media_previous: _create_bg_task(n.action_media_previous())
+        elif action == ACTION_BUTTON1 and n.action_button1: _create_bg_task(n.action_button1())
+        elif action == ACTION_BUTTON2 and n.action_button2: _create_bg_task(n.action_button2())
+        elif action == ACTION_BUTTON3 and n.action_button3: _create_bg_task(n.action_button3())
         n._result = action
         n._flag.set()
         if action in [ACTION_CLICK, ACTION_DELETE]:
@@ -268,17 +349,65 @@ class NotificationManager:
         except: pass
         asyncio.run(self.remove_all_notifications())
 
-DEFAULT_TMPDIR = "/data/data/com.termux/files/usr/tmp"
-DEFAULT_PATH = "/data/data/com.termux/files/usr/bin"
-TMPDIR = os.environ.get("TMPDIR", DEFAULT_TMPDIR) if _is_in_termux() else DEFAULT_TMPDIR
-PATH = os.environ.get("PATH", DEFAULT_PATH) if _is_in_termux() else DEFAULT_PATH
-TERMUX_TOAST = _find_in_path("termux-toast", PATH)
-TERMUX_NOTIFICATION = _find_in_path("termux-notification", PATH)
-TERMUX_NOTIFICATION_REMOVE = _find_in_path("termux-notification-remove", PATH)
-TERMUX_CLIPBOARD_GET = _find_in_path("termux-clipboard-get", PATH)
-TERMUX_CLIPBOARD_SET = _find_in_path("termux-clipboard-set", PATH)
-ADB = _find_in_path("adb", PATH)
-CURL = _find_in_path("curl", PATH)
+class Sensor:
+    def __init__(self, *sensors: str, delay_ms = 1000):
+        self.sensors = sensors
+        self.delay = delay_ms
+        self.proc: Optional[asyncio.subprocess.Process] = None
+    
+    async def read_once(self):
+        args = [TERMUX_SENSOR, "-s", ",".join(self.sensors), "-d", str(self.delay), "-n", "1"]
+        res = await _run(args)
+        result = res[1].decode("utf8").rstrip("\r\n") if res[1] else "{}"
+        return json.loads(result)
+    
+    async def __aenter__(self):
+        args = [TERMUX_SENSOR, "-s", ",".join(self.sensors), "-d", str(self.delay)]
+        cmd = shlex.join(args)
+        self.proc = await asyncio.create_subprocess_shell(
+            cmd,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        return SensorReader(self.proc.stdout)
+    
+    async def __aexit__(self, type, value, trace):
+        if self.proc:
+            print("    Cleaning Sensor...")
+            self.proc.stdin.write_eof()
+            await self.proc.stdin.drain()
+            self.proc.kill()
+            args = [TERMUX_SENSOR, "-c"]
+            await _run(args)
+            await self.proc.wait()
+            self.proc.stdin.close()
+            self.proc = None
+
+class SensorReader:
+    def __init__(self, reader: asyncio.StreamReader):
+        self.reader = reader
+        self.buffer = bytearray()
+        self.level = 0
+    
+    def __aiter__(self):
+        return self
+    
+    async def __anext__(self):
+        while True:
+            b = await self.reader.read(1)
+            if len(b) != 1:
+                raise StopAsyncIteration
+            byt = b[0]
+            self.buffer.append(byt)
+            if byt == BRACE_OPEN:
+                self.level += 1
+            elif byt == BRACE_CLOSE:
+                self.level -= 1
+                if self.level == 0:
+                    data = json.loads(self.buffer.decode("utf8"))
+                    self.buffer.clear()
+                    return data
+
 
 # ADB Functions Below
 
@@ -310,7 +439,7 @@ async def input_number_in_notification(nm: NotificationManager ,title="Input a n
     async def __click():
         data["confirmed"] = True
         flag.set()
-    n = Notification(nm.new_nid(), hint, title, sound=False, ongoing=False)
+    n = Notification(nm.new_nid(), hint, title, group=GROUP_NUMBER_INPUT, sound=False, ongoing=False)
     n.set_button1("+1", __add1)
     n.set_button2("+3", __add3)
     n.set_button3("Next Digit", __next)
@@ -328,7 +457,7 @@ async def input_number_in_notification(nm: NotificationManager ,title="Input a n
 
 async def adb_pair_local(nm: NotificationManager):
     code = await input_number_in_notification(nm, "Pairing Code")
-    port = await input_number_in_notification(nm, "Port")
+    port = await input_number_in_notification(nm, "Pairing Port")
     args = [ADB, "pair", f"127.0.0.1:{port}", f"{code}"]
     res = await _run(args)
     result = res[1].decode("utf8").lower() if res[1] else ""
@@ -337,7 +466,7 @@ async def adb_pair_local(nm: NotificationManager):
 async def adb_connect_local(nm: NotificationManager = None, port: Optional[int] = None):
     assert nm != None or port != None, "Must provide a port, or a NotificationManager to input a port."
     if not isinstance(port, int):
-        port = await input_number_in_notification(nm, "Port")
+        port = await input_number_in_notification(nm, "Wireless Debug Port")
     args = [ADB, "connect", f"127.0.0.1:{port}"]
     res = await _run(args)
     result = res[1].decode("utf8").lower() if res[1] else ""
@@ -360,6 +489,45 @@ async def adb_start_wireless_adb(port = 5555, reconnect = True):
         await adb_disconnect_all()
         await adb_connect_local(port=port)
 
+async def adb_start_wireless_adb_root(port = 5555, reconnect = True):
+    args = [SUDO, "setprop", "service.adb.tcp.port", str(port)]
+    await _run(args)
+    args = [SUDO, AD_STOP, "adbd"]
+    await _run(args)
+    args = [SUDO, AD_START, "adbd"]
+    await _run(args)
+    await asyncio.sleep(0.5)
+    if reconnect:
+        await adb_disconnect_all()
+        await adb_connect_local(port=port)
+
+async def adb_is_connect():
+    args = [ADB, "shell", "echo", "ok"]
+    res = await _run(args)
+    result = res[1].decode("utf8").strip() if res[1] else ""
+    return result == "ok"
+
+async def adb_try_connect(nm: NotificationManager, port = 5555):
+    connected = await adb_is_connect()
+    if connected:
+        # already connected
+        return True
+    await adb_disconnect_all()
+    await adb_start_wireless_adb_root(port, False)
+    connected = await adb_connect_local(port=port)
+    if connected:
+        # can be simple connected
+        return True
+    n = Notification(nm.new_nid(), "点击开始连接adb无线调试的流程, 你将要输入三个数字", "wait...")
+    await nm.send_notification_wait(n)
+    await asyncio.sleep(1)
+    await adb_pair_local(nm)
+    connected = await adb_connect_local(nm)
+    if not connected:
+        return False
+    await adb_start_wireless_adb(port)
+    return True
+
 async def adb_send_keyevent(key_code: int):
     """ Full Keycode: https://developer.android.com/reference/android/view/KeyEvent """
     await adb_shell(["input", "keyevent", str(key_code)])
@@ -369,6 +537,9 @@ async def adb_send_keyevent(key_code: int):
 async def __main():
     nm = NotificationManager()
     await nm.start_callback_server()
+    if not await adb_try_connect(nm, 5555):
+        print("adb配对连接失败, 请重试")
+        return
     n = Notification(nm.new_nid(), "点击这条通知，开始程序", "Yay!")
     n.set_button1("Hello")
     n.set_button2("Dragon")
@@ -376,20 +547,18 @@ async def __main():
     result = await nm.send_notification_wait(n)
     if result == ACTION_DELETE:
         return
-    connected = await adb_connect_local(port=5555)
-    if not connected:
-        n = Notification(nm.new_nid(), "点击开始连接adb无线调试的流程", "wait...")
-        await asyncio.sleep(500)
-        await adb_pair_local(nm)
-        await adb_connect_local(nm)
-        await adb_start_wireless_adb(5555)
-    await toast("好耶，我们不需要root权限，就可以在termux环境下使用adb连接本机了!", short=True)
-    await adb_send_keyevent(7) # 0
-    await adb_send_keyevent(8)
-    await adb_send_keyevent(9)
-    await adb_send_keyevent(10)
-    await adb_send_keyevent(11)
-    await adb_send_keyevent(12) # 5
+    await toast("好耶, 我们不需要root权, 就可以在termux环境下使用adb连接本机了!", short=True)
+    await adb_send_keyevent(3) # Home key
+    print(await sensor_list())
+    sensor = Sensor("linear_acc", "rot_vec", delay_ms=200)
+    async with sensor as reader:
+        # only one sensor object can read at the same time
+        count = 0
+        async for data in reader:
+            print(data)
+            count += 1
+            if count >= 10:
+                break
     nm.stop_callback_server()
 
 if __name__ == "__main__":
